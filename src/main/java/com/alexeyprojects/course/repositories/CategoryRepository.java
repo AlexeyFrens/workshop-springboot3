a0a2be665878940b3eae5429ed39a512ceef25d4
João Cláudio <leiloka46@gmail.com>
2024-08-29 20:00:10 -0300

    Category class

---
+++ src/main/java/com/alexeyprojects/course/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.alexeyprojects.course.repositories;
+
+import com.alexeyprojects.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}

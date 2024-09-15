3d7e1af57f5677acaceb3f9d863b30c6578ec6a6
João Cláudio <leiloka46@gmail.com>
2024-09-15 19:09:17 -0300

    Exception handling - update

--- src/main/java/com/alexeyprojects/course/services/UserService.java
+++ src/main/java/com/alexeyprojects/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.alexeyprojects.course.entities.User;
 import com.alexeyprojects.course.repositories.UserRepository;
 import com.alexeyprojects.course.services.exceptions.DatabaseException;
 import com.alexeyprojects.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User user){
-        User entity = repository.getReferenceById(id);  //Preparando um objeto monitorado
-        updateData(entity, user);
-        return repository.save(entity);
+        try{
+            User entity = repository.getReferenceById(id);  //Preparando um objeto monitorado
+            updateData(entity, user);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
